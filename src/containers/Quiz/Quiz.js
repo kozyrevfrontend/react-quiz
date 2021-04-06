fa051d342001442e5f09b81799e9ae22f8d3abde
Ilya <kozyrev.frontend@gmail.com>
2021-04-06 15:06:44 +0300

    добавляет компонент Quiz
    Quiz будет обладать стейтом и служить контейнером для всех вопросов

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Quiz.module.css'
+
+class Quiz extends React.Component {
+    render() {
+        return(
+            <div className={classes.Quiz}>
+                <h1>Quiz</h1>
+            </div>
+        )
+    }
+}
+
+export default Quiz
